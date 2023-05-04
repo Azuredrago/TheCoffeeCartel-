92de841a5b8a5f9ae32e899b276e845953c5c5b0
Md Adnan Hussain Ansari <adnanhansari007@gmail.com>
2023-05-04 09:30:36 +0530

    Review,Payment,Menu,contact done by Adnan

--- Cafe_Coffee_Day/coffee/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from coffee import views
-
-urlpatterns=[
-    path('',views.index,name="index"),
-    path('signup',views.signup,name="signup"),
-    path('handlelogin',views.handlelogin,name="handlelogin")
-
-]
